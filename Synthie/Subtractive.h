d0e6953f3272285a934197f77af18a652923cb9f
Scott Holzknecht <holzkne2@cse.msu.edu>
2015-03-11 14:01:33 -0400

    header

---
+++ Synthie/Subtractive.h
@@ -0,0 +1,28 @@
+/**
+ * \file Subtractive.h
+ *
+ * \author Scott Holzknecht
+ *
+ * \brief Subtractive synth class
+ */
+
+#pragma once
+
+#include "Instrument.h"
+
+/**
+ * \brief Subtractive synth class
+ */
+class CSubtractive :
+	public CInstrument
+{
+public:
+	CSubtractive();
+	/** \brief Copy constructor disabled */
+	CSubtractive(const CSubtractive &) = delete;
+	/** \brief Assignment operator disabled */
+	void operator=(const CSubtractive &) = delete;
+
+	virtual ~CSubtractive();
+};
+
