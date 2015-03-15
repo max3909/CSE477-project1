5c1332bb58ae19f3aa8f1cdabc8f5f49fe39e229
max3909 <max3909@gmail.com>
2015-03-15 00:11:26 -0400

    Reverb update

--- Synthie/Reverb.cpp
+++ Synthie/Reverb.cpp
@@ -23,19 +23,24 @@ bool CReverb::Generate(){
 }
 
 void CReverb::Process(double *frameIn, double *frameOut){
+	int delayMilliseconds = 500; 
+	int delaySamples =
+		(int)((float)delayMilliseconds * GetSampleRate()/100); 
+	float decay = 0.5f;
 	// Loop over the channels
 	for (int c = 0; c<2; c++)
 	{
 		m_queue[m_wrloc + c] = frameIn[c];
 
 		// Add output of the queue to the current input
-		frameOut[c] = m_dry * frameIn[c] + m_wet * m_queue[m_rdloc + c];
+		frameOut[c] = m_dry * frameIn[c];
+		frameOut[c + delaySamples] = m_wet * frameIn[c] * decay;
 	}
 
-	int flange = 0.006 + sin(0.25 * 2 * M_PI * m_time) * 0.004;
-	int delaylength = int((flange*GetSampleRate() + 0.5) * 2);
+	//int flange = 0.006 + sin(0.25 * 2 * M_PI * m_time) * 0.004;
+	//int delaylength = int((flange*GetSampleRate() + 0.5) * 2);
 	m_wrloc = (m_wrloc + 2) % QSIZE;
-	m_rdloc = (m_wrloc + QSIZE - delaylength) % QSIZE;
+	m_rdloc = (m_wrloc + 2) % QSIZE;
 }
 
 void CReverb::SetNote(CNote *note){
