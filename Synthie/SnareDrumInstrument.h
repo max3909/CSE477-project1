876dcd64cddfc9e57460d31cf3168404ae74621b
Sahil <tandonsa@msu.edu>
2015-03-17 14:47:02 -0400

    start of snare drum, waveplayer slomo

---
+++ Synthie/SnareDrumInstrument.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "Instrument.h"
+class CSnareDrumInstrument :
+	public CInstrument
+{
+public:
+	CSnareDrumInstrument();
+	virtual ~CSnareDrumInstrument();
+};
+
--- Synthie/WavePlayer.cpp
+++ Synthie/WavePlayer.cpp
@@ -25,27 +25,59 @@ void CWavePlayer::Start()
 
 void CWavePlayer::UpdatePos()
 {
-	m_position = m_position + m_pitchfactor + 1;
+	if (m_pitchfactor >= 0)
+		m_position = m_position + m_pitchfactor;
+	else
+		m_position++;
 }
 
 bool CWavePlayer::Generate()
 {
+	double m_prev_frame[2];
 
 	// Read the component's sample and make it our resulting frame.
 	if (m_time <  m_attack)
 	{
-		m_frame[0] = (m_samples[m_position++] / 32768.0) * m_time / m_attack;
-		m_frame[1] = (m_samples[m_position] / 32768.0) * m_time / m_attack;
-		UpdatePos();
-		m_time += GetSamplePeriod();
+		if (m_pitchfactor > 0)
+		{
+			m_frame[0] = (m_samples[m_position++] / 32768.0) * m_time / m_attack;
+			m_frame[1] = (m_samples[m_position] / 32768.0) * m_time / m_attack;
+			UpdatePos();
+			m_time += GetSamplePeriod();
+		}
+		else
+		{
+			m_frame[0] = (0.5 * (m_samples[m_position++] + m_samples[m_position - 2]) / 32768.0) * m_time / m_attack;
+			m_frame[1] = (0.5 * (m_samples[m_position] + m_samples[m_position - 2]) / 32768.0) * m_time / m_attack;
+			if (m_position % 2 == 0)
+			{
+				UpdatePos();
+				m_time += GetSamplePeriod();
+			}
+		}
+		
 		return true;
 	}
 	else if (m_time > m_duration - m_release && m_time < m_duration)
 	{
-		m_frame[0] = (m_samples[m_position++] / 32768.0) * (m_duration - m_time) / m_release;
-		m_frame[1] = (m_samples[m_position] / 32768.0) * (m_duration - m_time) / m_release;
-		UpdatePos();
-		m_time += GetSamplePeriod();
+		if (m_pitchfactor > 0)
+		{
+			m_frame[0] = (m_samples[m_position++] / 32768.0) * (m_duration - m_time) / m_release;
+			m_frame[1] = (m_samples[m_position] / 32768.0) * (m_duration - m_time) / m_release;
+			UpdatePos();
+			m_time += GetSamplePeriod();
+		}
+		else
+		{
+			m_frame[0] = (0.5 * (m_samples[m_position++] + m_samples[m_position - 2]) / 32768.0) * (m_duration - m_time) / m_release;
+			m_frame[1] = (0.5 * (m_samples[m_position] + m_samples[m_position - 2]) / 32768.0) * (m_duration - m_time) / m_release;
+			if (m_position % 2 == 0)
+			{
+				UpdatePos();
+				m_time += GetSamplePeriod();
+			}
+		}
+		
 		return true;
 	}
 	else
@@ -53,11 +85,25 @@ bool CWavePlayer::Generate()
 
 		if (m_position < m_numsamples && m_time < m_duration)
 		{
-			m_frame[0] = m_samples[m_position++] / 32768.0;
-			m_frame[1] = m_samples[m_position] / 32768.0;
-			UpdatePos();
+			if (m_pitchfactor > 0)
+			{
+				m_frame[0] = m_samples[m_position++] / 32768.0;
+				m_frame[1] = m_samples[m_position] / 32768.0;
+				UpdatePos();
+			}
+			else
+			{
+				m_frame[0] = 0.5 * (m_samples[m_position++] + m_samples[m_position - 2]) / 32768.0;
+				m_frame[1] = 0.5 * (m_samples[m_position] + m_samples[m_position - 2]) / 32768.0;
+				if (m_position % 2 == 0)
+				{
+					UpdatePos();
+					m_time += GetSamplePeriod();
+				}
+			}
+			
 
-			m_time += GetSamplePeriod();
+			
 			return true;
 		}
 		else
