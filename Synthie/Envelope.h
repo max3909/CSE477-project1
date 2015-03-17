d336213387e8cf24a5b96d57ca8845934771eb4a
Scott Holzknecht <holzkne2@cse.msu.edu>
2015-03-16 22:55:04 -0400

    and the header

---
+++ Synthie/Envelope.h
@@ -0,0 +1,36 @@
+/**
+ * \file Envelope.h
+ *
+ * \author Scott Holzknecht
+ *
+ * \brief ADSR generator
+ */
+
+#pragma once
+#include "Subtractive.h"
+
+/**
+ * \brief ADSR generator
+ */
+class CEnvelope :
+	public CSubtractive
+{
+public:
+	CEnvelope();
+
+	/** \brief Copy constructor disabled */
+	CEnvelope(const CEnvelope &) = delete;
+	/** \brief Assignment operator disabled */
+	void operator=(const CEnvelope &) = delete;
+
+	virtual ~CEnvelope();
+
+	virtual void Start() override;
+	virtual bool Generate() override;
+
+private:
+	double m_attackend;
+	double m_delayend;
+	double m_releasestart;
+};
+
