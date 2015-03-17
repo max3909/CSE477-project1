fa3cda512a097008a2ef8e716f4be97d26986b65
Scott Holzknecht <holzkne2@cse.msu.edu>
2015-03-16 22:54:53 -0400

    Envelope class

---
+++ Synthie/Envelope.cpp
@@ -0,0 +1,29 @@
+/**
+ * \file Envelope.cpp
+ *
+ * \author Scott Holzknecht
+ */
+
+#include "stdafx.h"
+#include "Envelope.h"
+
+
+CEnvelope::CEnvelope()
+{
+}
+
+
+CEnvelope::~CEnvelope()
+{
+}
+
+void CEnvelope::Start()
+{
+
+}
+
+bool CEnvelope::Generate()
+{
+
+	return false;
+}
