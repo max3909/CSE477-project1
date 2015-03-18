f22c3da5e6a686d8d6c59d340089128bd4048ec1
DoctorWizard <nelso475@msu.edu>
2015-03-17 21:01:47 -0400

    time duration updated for pith grater taan tonennkansaldd lj

--- Synthie/WavePlayer.cpp
+++ Synthie/WavePlayer.cpp
@@ -95,6 +95,7 @@ bool CWavePlayer::Generate()
 				m_frame[0] = m_samples[m_position++] / 32768.0;
 				m_frame[1] = m_samples[m_position] / 32768.0;
 				UpdatePos();
+				m_time += GetSamplePeriod();
 			}
 			else
 			{
